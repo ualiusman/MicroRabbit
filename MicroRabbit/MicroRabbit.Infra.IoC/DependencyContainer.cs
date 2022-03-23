1c3ab752cb4d401f693c0553763e576ccdf6607a
ualiusman <usmanaliua.uali@gmail.com>
2022-03-23 17:48:34 +0500

    RabbitMQ bus changes

---
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using MicroRabbit.Infa.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using MiroRabbit.Domain.Core.Bus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection service)
+        {
+            service.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
